7e94b1d0a7c19e33d44e4b07a6fb4aaace644f34
Adeyinka <adeyinkamichael95@gmail.com>
2020-02-06 10:16:06 -0800

    remove

--- pictures/settings.py
+++ pictures/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'pictures.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pictures',
-        'USER': 'postgres',
-        'PASSWORD': 'django1234',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
