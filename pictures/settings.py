b0591bfce44ac3912535b8b04f88dd3efe8148a5
Adeyinka <adeyinkamichael95@gmail.com>
2020-02-07 09:33:42 -0800

    delected

--- pictures/settings.py
+++ pictures/settings.py
@@ -77,13 +77,11 @@ WSGI_APPLICATION = 'pictures.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'picture',
-        'PASSWORD': 'django1234',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
